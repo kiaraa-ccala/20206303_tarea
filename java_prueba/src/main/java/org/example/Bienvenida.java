8995f1c36fdd8f34c221b94f1fcb0d4182e81ab7
kiaraa <kiaraa.ccala@pucp.edu.pe>
2024-10-30 21:24:09 -0500

    guardando ando

--- java_prueba/src/main/java/org/example/Bienvenida.java
+++ java_prueba/src/main/java/org/example/Bienvenida.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class Main {
+public class Bienvenida {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido, Kiara");
     }
 }
